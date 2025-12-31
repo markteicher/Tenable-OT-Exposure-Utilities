f8791da2b7981144cc722815004961ae645d9b5c
Mark Teicher <mark.teicher@gmail.com>
2025-12-31 07:52:47 -0500

    Create basic_credentials_with_role_types.py

---
+++ queries/basic_credentials_with_role_types.py
@@ -0,0 +1,21 @@
+"""
+queries/basic_credentials_with_role_types.py
+
+Tenable OT Security – BasicCredentialsWithRoleTypes (ENUM)
+
+Source documentation:
+https://docs.tenable.com/OT-security/api/basiccredentialswithroletypes.doc.html
+
+Purpose:
+- Enumerates credential types that REQUIRE an explicit role binding
+- Used by role-based authentication mutations
+- This enum is intentionally small by design
+
+IMPORTANT:
+- This file is a literal transcription of the GraphQL enum
+- Do NOT add, infer, or merge values from other credential enums
+"""
+
+BASIC_CREDENTIALS_WITH_ROLE_TYPES = [
+    "AbbRtu500",
+]
