76289ea4453c97b003f17b4bf77794c0e9bdae27
Mark Teicher <mark.teicher@gmail.com>
2025-12-31 16:15:30 -0500

    Create custom_field_value_type.py

---
+++ queries/custom_field_value_type.py
@@ -0,0 +1,18 @@
+"""
+queries/custom_field_value_type.py
+
+Tenable OT Security – CustomFieldValueType GraphQL Enum
+
+Source documentation:
+https://docs.tenable.com/OT-security/api/customfieldvaluetype.doc.html
+
+Purpose:
+- Enumerates supported value types for custom fields
+"""
+
+CUSTOM_FIELD_VALUE_TYPE_ENUM_NAME = "CustomFieldValueType"
+
+CUSTOM_FIELD_VALUE_TYPES = [
+    "PlainText",
+    "HyperLink",
+]
