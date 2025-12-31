53f02300db921ecd81bd04ec8099894cc34dab55
Mark Teicher <mark.teicher@gmail.com>
2025-12-31 10:36:20 -0500

    Create channel_type.py

---
+++ queries/channel_type.py
@@ -0,0 +1,19 @@
+"""
+queries/channel_type.py
+
+Tenable OT Security – ChannelType GraphQL Enum
+
+Source documentation:
+https://docs.tenable.com/OT-security/api/channeltype.doc.html
+
+Purpose:
+- Enumerates ChannelType values used by DhPlusDetails relationships
+"""
+
+CHANNEL_TYPE_ENUM_NAME = "ChannelType"
+
+CHANNEL_TYPES = [
+    "Unknown",
+    "ChannelA",
+    "ChannelB",
+]
