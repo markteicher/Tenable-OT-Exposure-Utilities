c7135f554af938d273d749efed43b34fcb2ef263
Mark Teicher <mark.teicher@gmail.com>
2025-12-31 13:46:53 -0500

    Create concurrent_workers_options.py

---
+++ queries/concurrent_workers_options.py
@@ -0,0 +1,19 @@
+"""
+queries/concurrent_workers_options.py
+
+Tenable OT Security – ConcurentWorkersOptions GraphQL Enum
+
+Source documentation:
+https://docs.tenable.com/OT-security/api/concurentworkersoptions.doc.html
+
+Purpose:
+- Defines allowed concurrent worker limits for GraphQL operations
+"""
+
+CONCURRENT_WORKERS_OPTIONS_ENUM_NAME = "ConcurentWorkersOptions"
+
+CONCURRENT_WORKERS_OPTIONS = [
+    "Ten",
+    "Twenty",
+    "Thirty",
+]
