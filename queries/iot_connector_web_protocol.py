e11dcea054b966c798ea27a43d5682bf52dbed12
Mark Teicher <mark.teicher@gmail.com>
2026-01-01 18:53:06 -0500

    Add IoT connector web protocol enumeration
    Defines an enum for supported IoT connector web protocols.

---
+++ queries/iot_connector_web_protocol.py
@@ -0,0 +1,18 @@
+"""
+queries/iot_connector_web_protocol.py
+
+Tenable OT Security – IotConnectorWebProtocol GraphQL Enum
+
+Source documentation:
+https://docs.tenable.com/OT-security/api/iotconnectorwebprotocol.doc.html
+
+Purpose:
+- Enumerates supported web protocols for IoT connectors
+"""
+
+IOT_CONNECTOR_WEB_PROTOCOL_ENUM_NAME = "IotConnectorWebProtocol"
+
+IOT_CONNECTOR_WEB_PROTOCOLS = [
+    "HTTP",
+    "HTTPS",
+]
