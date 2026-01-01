5358140280ff7a354f3e3e8a76775bf1af4ae937
Mark Teicher <mark.teicher@gmail.com>
2026-01-01 17:28:40 -0500

    Add IDSGeneralNewEvent GraphQL Enum
    Defines an enumeration for new IDS General events.

---
+++ queries/ids_general_new_event.py
@@ -0,0 +1,17 @@
+"""
+queries/ids_general_new_event.py
+
+Tenable OT Security – IDSGeneralNewEvent GraphQL Enum
+
+Source documentation:
+https://docs.tenable.com/OT-security/api/idsgeneralnewevent.doc.html
+
+Purpose:
+- Enumerates possible events for new/edit IDS General policies
+"""
+
+IDS_GENERAL_NEW_EVENT_ENUM_NAME = "IDSGeneralNewEvent"
+
+IDS_GENERAL_NEW_EVENTS = [
+    "IpConflict",
+]
