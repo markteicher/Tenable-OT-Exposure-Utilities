0292597d8262f533f97479dcf7f5316218bada17
Mark Teicher <mark.teicher@gmail.com>
2026-01-01 18:45:47 -0500

    Add IotConnectionMethod GraphQL Enum
    Defines an enumeration for supported IoT connection methods.

---
+++ queries/iot_connection_method.py
@@ -0,0 +1,18 @@
+"""
+queries/iot_connection_method.py
+
+Tenable OT Security – IotConnectionMethod GraphQL Enum
+
+Source documentation:
+https://docs.tenable.com/OT-security/api/iotconnectionmethod.doc.html
+
+Purpose:
+- Enumerates supported IoT connection methods
+"""
+
+IOT_CONNECTION_METHOD_ENUM_NAME = "IotConnectionMethod"
+
+IOT_CONNECTION_METHODS = [
+    "RemoteAgent",
+    "RemoteApi",
+]
