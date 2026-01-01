aac03467337b9715181ccd061a0bc9ded232a673
Mark Teicher <mark.teicher@gmail.com>
2026-01-01 18:43:34 -0500

    Add IntrusionPolicyEvent GraphQL Enum definition
    Defines the IntrusionPolicyEvent GraphQL Enum for Tenable OT Security, enumerating possible events for intrusion policies.

---
+++ queries/intrusion_policy_event.py
@@ -0,0 +1,17 @@
+"""
+queries/intrusion_policy_event.py
+
+Tenable OT Security – IntrusionPolicyEvent GraphQL Enum
+
+Source documentation:
+https://docs.tenable.com/OT-security/api/intrusionpolicyevent.doc.html
+
+Purpose:
+- Enumerates possible events for intrusion policies
+"""
+
+INTRUSION_POLICY_EVENT_ENUM_NAME = "IntrusionPolicyEvent"
+
+INTRUSION_POLICY_EVENTS = [
+    "IntrusionDetected",
+]
