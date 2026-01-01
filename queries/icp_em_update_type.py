c69d5e6763ccb455d74499631d22c9afbff115b4
Mark Teicher <mark.teicher@gmail.com>
2026-01-01 16:38:14 -0500

    Add IcpEmUpdateType GraphQL Enum definition
    Defines a GraphQL Enum for ICP-EM update types.

---
+++ queries/icp_em_update_type.py
@@ -0,0 +1,17 @@
+"""
+queries/icp_em_update_type.py
+
+Tenable OT Security – IcpEmUpdateType GraphQL Enum
+
+Source documentation:
+https://docs.tenable.com/OT-security/api/icpemupdatetype.doc.html
+
+Purpose:
+- Enumerates ICP-EM update types
+"""
+
+ICP_EM_UPDATE_TYPE_ENUM_NAME = "IcpEmUpdateType"
+
+ICP_EM_UPDATE_TYPES = [
+    "SecurityPatch",
+    "ApplicationUpdate
