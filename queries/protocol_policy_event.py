68b8199aed8658a04c73ed4fd65fcdac3fae01d1
Mark Teicher <mark.teicher@gmail.com>
2026-01-01 22:47:53 -0500

    Create protocol_policy_event.py

---
+++ queries/protocol_policy_event.py
@@ -0,0 +1,16 @@
+# queries/protocol_policy_event.py
+#
+# Tenable OT Security – ProtocolPolicyEvent GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/protocolpolicyevent.doc.html
+#
+# Purpose:
+# - Enumerates possible events for Network Policies
+
+PROTOCOL_POLICY_EVENT_ENUM_NAME = "ProtocolPolicyEvent"
+
+PROTOCOL_POLICY_EVENTS = [
+    "Conversation",
+    "BaselineDeviation",
+]
