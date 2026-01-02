6721593a4237988600327f3f6acb18ca21bc1c02
Mark Teicher <mark.teicher@gmail.com>
2026-01-01 19:23:15 -0500

    Add NetworkHostsSummaryDirection GraphQL Enum
    Defines the NetworkHostsSummaryDirection GraphQL Enum for Tenable OT Security.

---
+++ queries/network_hosts_summary_direction.py
@@ -0,0 +1,18 @@
+"""
+queries/network_hosts_summary_direction.py
+
+Tenable OT Security – NetworkHostsSummaryDirection GraphQL Enum
+
+Source documentation:
+https://docs.tenable.com/OT-security/api/networkhostssummarydirection.doc.html
+
+Purpose:
+- Specifies which end of the network conversation is shown in host summaries
+"""
+
+NETWORK_HOSTS_SUMMARY_DIRECTION_ENUM_NAME = "NetworkHostsSummaryDirection"
+
+NETWORK_HOSTS_SUMMARY_DIRECTIONS = [
+    "source",
+    "destination",
+]
