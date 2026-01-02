0f42124e816b48248832273dd20ba82f104be501
Mark Teicher <mark.teicher@gmail.com>
2026-01-01 20:40:20 -0500

    Add PauseBetweenProbesOptions GraphQL Enum
    Defines an enumeration for pause duration options between active probes in Tenable OT Security.

---
+++ queries/pause_between_probes_options.py
@@ -0,0 +1,19 @@
+"""
+queries/pause_between_probes_options.py
+
+Tenable OT Security – PauseBetweenProbesOptions GraphQL Enum
+
+Source documentation:
+https://docs.tenable.com/OT-security/api/pausebetweenprobesoptions.doc.html
+
+Purpose:
+- Enumerates pause duration options between active probes
+"""
+
+PAUSE_BETWEEN_PROBES_OPTIONS_ENUM_NAME = "PauseBetweenProbesOptions"
+
+PAUSE_BETWEEN_PROBES_OPTIONS = [
+    "OneSecond",
+    "TwoSeconds",
+    "ThreeSeconds",
+]
