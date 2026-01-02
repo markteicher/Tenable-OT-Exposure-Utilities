c9b56766c5f9515429ce591c83406f38217dffe5
Mark Teicher <mark.teicher@gmail.com>
2026-01-01 23:01:25 -0500

    Create queries_category.py

---
+++ queries/queries_category.py
@@ -0,0 +1,17 @@
+# queries/queries_category.py
+#
+# Tenable OT Security – QueriesCategory GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/queriescategory.doc.html
+#
+# Purpose:
+# - Categorizes saved queries by domain (IT, OT, Discovery)
+
+QUERIES_CATEGORY_ENUM_NAME = "QueriesCategory"
+
+QUERIES_CATEGORIES = [
+    "IT",
+    "OT",
+    "Discovery",
+]
