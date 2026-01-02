b1ace846812ed51fd69d700c1d2eb6d34c4d972d
Mark Teicher <mark.teicher@gmail.com>
2026-01-02 14:34:59 -0500

    Create sensitivity.py

---
+++ queries/sensitivity.py
@@ -0,0 +1,17 @@
+# queries/sensitivity.py
+#
+# Tenable OT Security – Sensitivity GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/sensitivity.doc.html
+#
+# Purpose:
+# - Represents the sensitivity level used by spike definitions and parameters
+
+SENSITIVITY_ENUM_NAME = "Sensitivity"
+
+SENSITIVITY = [
+    "Low",
+    "Medium",
+    "High",
+]
