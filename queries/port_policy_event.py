7234b33d9c3dac42f5d68166ab6b5037a3d76123
Mark Teicher <mark.teicher@gmail.com>
2026-01-01 22:42:27 -0500

    Create port_policy_event.py

---
+++ queries/port_policy_event.py
@@ -0,0 +1,15 @@
+# queries/port_policy_event.py
+#
+# Tenable OT Security – PortPolicyEvent GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/portpolicyevent.doc.html
+#
+# Purpose:
+# - Enumerates possible events for Port Policy
+
+PORT_POLICY_EVENT_ENUM_NAME = "PortPolicyEvent"
+
+PORT_POLICY_EVENTS = [
+    "OpenPort",
+]
