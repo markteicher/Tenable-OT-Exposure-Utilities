b7dae6780a83f9860e5b993125337dea9f8bdc87
Mark Teicher <mark.teicher@gmail.com>
2026-01-01 19:37:04 -0500

    Add OtAgentScanResult GraphQL Enum
    Defines an enumeration for OT Agent scan results.

---
+++ queries/ot_agent_scan_result.py
@@ -0,0 +1,18 @@
+"""
+queries/ot_agent_scan_result.py
+
+Tenable OT Security – OtAgentScanResult GraphQL Enum
+
+Source documentation:
+https://docs.tenable.com/OT-security/api/otagentscanresult.doc.html
+
+Purpose:
+- Enumerates possible results of an OT Agent scan execution
+"""
+
+OT_AGENT_SCAN_RESULT_ENUM_NAME = "OtAgentScanResult"
+
+OT_AGENT_SCAN_RESULTS = [
+    "Failed",
+    "Completed",
+]
