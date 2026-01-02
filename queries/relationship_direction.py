5a876beb8ca25087b85aa63eeb9f81d297785d56
Mark Teicher <mark.teicher@gmail.com>
2026-01-02 06:00:51 -0500

    Create relationship_direction.py

---
+++ queries/relationship_direction.py
@@ -0,0 +1,17 @@
+# queries/relationship_direction.py
+#
+# Tenable OT Security – RelationshipDirection GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/relationshipdirection.doc.html
+#
+# Purpose:
+# - Defines directionality of relationships between entities
+
+RELATIONSHIP_DIRECTION_ENUM_NAME = "RelationshipDirection"
+
+RELATIONSHIP_DIRECTIONS = [
+    "Bidirectional",
+    "From",
+    "To",
+]
