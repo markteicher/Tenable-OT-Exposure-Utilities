336f67195f4125a5ceedb5247174dd59a2c01a08
Mark Teicher <mark.teicher@gmail.com>
2026-01-01 19:03:08 -0500

    Create license_type.py for LicenseType Enum
    Defines a GraphQL Enum for Tenable OT license types.

---
+++ queries/license_type.py
@@ -0,0 +1,19 @@
+"""
+queries/license_type.py
+
+Tenable OT Security – LicenseType GraphQL Enum
+
+Source documentation:
+https://docs.tenable.com/OT-security/api/licensetype.doc.html
+
+Purpose:
+- Defines the type of Tenable OT license
+"""
+
+LICENSE_TYPE_ENUM_NAME = "LicenseType"
+
+LICENSE_TYPE_VALUES = [
+    "UnknownLicenseType",
+    "Subscription",
+    "Perpetual",
+]
