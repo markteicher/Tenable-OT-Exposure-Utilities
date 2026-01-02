d21c2229c9d50e818f941498b8a83d361b395bbc
Mark Teicher <mark.teicher@gmail.com>
2026-01-02 06:11:11 -0500

    Create resolution_type.py

---
+++ queries/resolution_type.py
@@ -0,0 +1,16 @@
+# queries/resolution_type.py
+#
+# Tenable OT Security – ResolutionType GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/resolutiontype.doc.html
+#
+# Purpose:
+# - The possible resolutions of the system
+
+RESOLUTION_TYPE_ENUM_NAME = "ResolutionType"
+
+RESOLUTION_TYPE = [
+    "Daily",
+    "Interval",
+]
