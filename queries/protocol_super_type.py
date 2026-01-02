66e28d2ee7da7bc28f64f66d1840caf5321541dd
Mark Teicher <mark.teicher@gmail.com>
2026-01-01 22:52:41 -0500

    Create protocol_super_type.py

---
+++ queries/protocol_super_type.py
@@ -0,0 +1,63 @@
+# queries/protocol_super_type.py
+#
+# Tenable OT Security – ProtocolSuperType GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/protocolsupertype.doc.html
+#
+# Purpose:
+# - Enumerates protocol super types for network and policy queries
+
+PROTOCOL_SUPER_TYPE_ENUM_NAME = "ProtocolSuperType"
+
+PROTOCOL_SUPER_TYPES = [
+    "UNKNOWN",
+    "TCP",
+    "UDP",
+    "MODBUS",
+    "UNITY",
+    "CONCEPT",
+    "PROFINET",
+    "CIP",
+    "PCCC",
+    "ETHIP",
+    "LLC",
+    "S7",
+    "S7Plus",
+    "P2",
+    "SRTP",
+    "DELTAV",
+    "MMS",
+    "ROC",
+    "FTE",
+    "DNS",
+    "BROWSER",
+    "DIGSI4",
+    "SICAM_PROFIBUS",
+    "IEC61850",
+    "IEC104",
+    "YOKOGAWA_CENTUM",
+    "BACNET",
+    "LLDP",
+    "MELSEC",
+    "MASTERBUS300",
+    "FOXBORO",
+    "TOYOPUC",
+    "DNP3",
+    "COGNEX",
+    "ADS",
+    "MOXA",
+    "BACHMANN",
+    "FOX",
+    "HART_IP",
+    "OVATION",
+    "ABB_NC",
+    "SAIA",
+    "PCOM",
+    "BOSCH_PSI",
+    "WAGO",
+    "ION",
+    "PROFINET_CM",
+    "PHOENIX_CONTACT",
+    "SMARTX",
+]
