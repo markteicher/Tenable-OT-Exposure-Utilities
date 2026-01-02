c8debb2bbdaf6ce931bb38c09555e79b646a0911
Mark Teicher <mark.teicher@gmail.com>
2026-01-02 08:33:25 -0500

    Create return_type.py

---
+++ queries/return_type.py
@@ -0,0 +1,19 @@
+# queries/return_type.py
+#
+# Tenable OT Security – ReturnType GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/returntype.doc.html
+#
+# Purpose:
+# - Defines the return data type for query results
+
+RETURN_TYPE_ENUM_NAME = "ReturnType"
+
+RETURN_TYPE = [
+    "Boolean",
+    "Integer",
+    "Float",
+    "String",
+    "Time",
+]
