937194b2d125b08555a4d1274c954d8f0e402db1
Mark Teicher <mark.teicher@gmail.com>
2026-01-02 05:57:22 -0500

    Create rediscovered_asset_event.py

---
+++ queries/rediscovered_asset_event.py
@@ -0,0 +1,15 @@
+# queries/rediscovered_asset_event.py
+#
+# Tenable OT Security – RediscoveredAssetEvent GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/rediscoveredassetevent.doc.html
+#
+# Purpose:
+# - Enumerates possible events for a rediscovered asset
+
+REDISCOVERED_ASSET_EVENT_ENUM_NAME = "RediscoveredAssetEvent"
+
+REDISCOVERED_ASSET_EVENTS = [
+    "RediscoveredAsset",
+]
