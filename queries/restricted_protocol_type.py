f83e72ee96c2ee3be23941cf4fcb292759d3e8cb
Mark Teicher <mark.teicher@gmail.com>
2026-01-02 06:17:36 -0500

    Create restricted_protocol_type.py

---
+++ queries/restricted_protocol_type.py
@@ -0,0 +1,67 @@
+# queries/restricted_protocol_type.py
+#
+# Tenable OT Security – RestrictedProtocolType GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/restrictedprotocoltype.doc.html
+#
+# Purpose:
+# - Protocols that are considered restricted by the system
+
+RESTRICTED_PROTOCOL_TYPE_ENUM_NAME = "RestrictedProtocolType"
+
+RESTRICTED_PROTOCOL_TYPE = [
+    "MMS",
+    "ABB_RTUS00_HTTPS",
+    "PCCC_ABETH",
+    "ADS_UDP",
+    "ARP",
+    "BACHMANN_TCP",
+    "BACNET",
+    "CIP_ETHIP",
+    "CIP_ETHIP_OMRON",
+    "CIP_ETHIP_DHPLUS",
+    "COGNEX_DISCOVERY",
+    "DNS",
+    "FANUC_FTP",
+    "FANUC_HTTP",
+    "FOX",
+    "FOX_TLS",
+    "FTP",
+    "CEE",
+    "HTTP",
+    "IEC_61850",
+    "KEYENCE_HTTP",
+    "MELSEC_IQR_ETH_TCP",
+    "MELSEC_IQR_CPU_TCP",
+    "MELSEC_Q_CPU_TCP",
+    "MELSEC_Q_ETH_TCP",
+    "MELSEC_FIND",
+    "MODBUS_MODICON_TCP",
+    "MODBUS_UMAS_TCP",
+    "NBNS",
+    "OVATION",
+    "PCCC_ETHIP",
+    "PCOM",
+    "PROCONOS",
+    "PC_WORX",
+    "ICMPV4",
+    "PROFINET_CM",
+    "ROC",
+    "S7_TCP",
+    "S7_PLUS_TCP",
+    "SAIA",
+    "SEL_PSQL",
+    "SICAM_A8000_HTTP",
+    "SICAM_A8000_HTTPS",
+    "SICAM_HTTP",
+    "SICAM_HTTPS",
+    "SICAM_SNMP",
+    "SICAM_PROFIBUS",
+    "SIPROTEC4",
+    "SIPROTEC5_HTTPS",
+    "SNMP",
+    "SRTP",
+    "TOYOPUC",
+    "WMI",
+]
