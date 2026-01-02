fa3b4415c33ba5d50de79b6e4c6743b0b7555751
Mark Teicher <mark.teicher@gmail.com>
2026-01-02 14:37:10 -0500

    Create sensor_active_action.py

---
+++ queries/sensor_active_action.py
@@ -0,0 +1,16 @@
+# queries/sensor_active_action.py
+#
+# Tenable OT Security – SensorActiveAction GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/sensoractiveaction.doc.html
+#
+# Purpose:
+# - Actions that can be applied to a sensor’s active state
+
+SENSOR_ACTIVE_ACTION_ENUM_NAME = "SensorActiveAction"
+
+SENSOR_ACTIVE_ACTION = [
+    "Enable",
+    "Disable",
+]
