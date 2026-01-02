789c1711c2661769862663f1d31ae01c3a9b51d7
Mark Teicher <mark.teicher@gmail.com>
2026-01-01 20:37:51 -0500

    Add PasswordOnlyCredentialsTypes GraphQL Enum
    Defines a GraphQL enum for password-only credential types.

---
+++ queries/password_only_credentials_types.py
@@ -0,0 +1,18 @@
+"""
+queries/password_only_credentials_types.py
+
+Tenable OT Security – PasswordOnlyCredentialsTypes GraphQL Enum
+
+Source documentation:
+https://docs.tenable.com/OT-security/api/passwordonlycredentialstypes.doc.html
+
+Purpose:
+- Enumerates credential types that use password-only authentication
+"""
+
+PASSWORD_ONLY_CREDENTIALS_TYPES_ENUM_NAME = "PasswordOnlyCredentialsTypes"
+
+PASSWORD_ONLY_CREDENTIALS_TYPES = [
+    "Concept",
+    "Siprotec5",
+]
