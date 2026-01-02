d5af39eff87a2e89d45d881ae1bba8ca7ecdcb94
Mark Teicher <mark.teicher@gmail.com>
2026-01-02 14:19:53 -0500

    Create rule_group_type.py

---
+++ queries/rule_group_type.py
@@ -0,0 +1,15 @@
+# queries/rule_group_type.py
+#
+# Tenable OT Security – RuleGroupType GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/rulegrouptype.doc.html
+#
+# Purpose:
+# - Defines the type of rule group used in policies
+
+RULE_GROUP_TYPE_ENUM_NAME = "RuleGroupType"
+
+RULE_GROUP_TYPE = [
+    "RuleList",
+]
