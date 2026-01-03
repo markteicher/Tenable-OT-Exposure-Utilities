951741fcb32b4c7f4e6f7c5b702282d2307cfb35
Mark Teicher <mark.teicher@gmail.com>
2026-01-02 23:02:42 -0500

    Create snmpv2_credentials_types.py

---
+++ queries/snmpv2_credentials_types.py
@@ -0,0 +1,15 @@
+# queries/snmpv2_credentials_types.py
+#
+# Tenable OT Security – SnmpV2CredentialsTypes GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/snmpv2credentialstypes.doc.html
+#
+# Purpose:
+# - Defines supported SNMP v2 credential schema types
+
+SNMP_V2_CREDENTIALS_TYPES_ENUM_NAME = "SnmpV2CredentialsTypes"
+
+SNMP_V2_CREDENTIALS_TYPES = [
+    "SnmpV2",
+]
