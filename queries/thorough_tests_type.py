cce17228e4b4889525cdd8278bc615cb582f8b52
Mark Teicher <mark.teicher@gmail.com>
2026-01-04 13:05:34 -0500

    Create thorough_tests_type.py

---
+++ queries/thorough_tests_type.py
@@ -0,0 +1,16 @@
+# queries/thorough_tests_type.py
+#
+# Tenable OT Security – thoroughTestsType GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/thoroughteststype.doc.html
+#
+# Purpose:
+# - Indicates whether thorough tests are enabled
+
+THOROUGH_TESTS_TYPE_ENUM_NAME = "thoroughTestsType"
+
+THOROUGH_TESTS_TYPE = [
+    "No",
+    "Yes",
+]
