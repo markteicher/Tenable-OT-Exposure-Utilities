20b8b9edcfc2155d1fec396fe46ad881c37c0840
Mark Teicher <mark.teicher@gmail.com>
2026-01-04 16:02:25 -0500

    Create verbosity_type.py

---
+++ queries/verbosity_type.py
@@ -0,0 +1,16 @@
+# queries/verbosity_type.py
+#
+# Tenable OT Security – VerbosityType GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/verbositytype.doc.html
+#
+# Purpose:
+# - Controls the verbosity level of responses or operations
+
+VERBOSITY_TYPE_ENUM_NAME = "VerbosityType"
+
+VERBOSITY_TYPE = [
+    "Normal",
+    "Verbose",
+]
