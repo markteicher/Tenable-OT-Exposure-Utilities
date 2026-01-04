f7416f812d1768a43c738e2225145fd101b807a0
Mark Teicher <mark.teicher@gmail.com>
2026-01-04 10:14:36 -0500

    Create tag_group_type.py

---
+++ queries/tag_group_type.py
@@ -0,0 +1,15 @@
+# queries/tag_group_type.py
+#
+# Tenable OT Security – TagGroupType GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/taggrouptype.doc.html
+#
+# Purpose:
+# - Defines the type of tag group used by the system
+
+TAG_GROUP_TYPE_ENUM_NAME = "TagGroupType"
+
+TAG_GROUP_TYPE = [
+    "AssetsTags",
+]
