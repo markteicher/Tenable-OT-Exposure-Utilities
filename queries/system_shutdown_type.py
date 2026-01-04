01ec24d83c43463fdaacc9494eb11ccaae632c14
Mark Teicher <mark.teicher@gmail.com>
2026-01-04 10:09:38 -0500

    Create system_shutdown_type.py

---
+++ queries/system_shutdown_type.py
@@ -0,0 +1,16 @@
+# queries/system_shutdown_type.py
+#
+# Tenable OT Security – SystemShutdownType GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/systemshutdowntype.doc.html
+#
+# Purpose:
+# - Defines which power action to perform on a system
+
+SYSTEM_SHUTDOWN_TYPE_ENUM_NAME = "SystemShutdownType"
+
+SYSTEM_SHUTDOWN_TYPE = [
+    "Reboot",
+    "Shutdown",
+]
