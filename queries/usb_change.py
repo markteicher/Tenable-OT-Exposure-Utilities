09e1efd7b630e6bce69a44d7a18ec4837bd6e457
Mark Teicher <mark.teicher@gmail.com>
2026-01-04 13:20:17 -0500

    Create usb_change.py

---
+++ queries/usb_change.py
@@ -0,0 +1,16 @@
+# queries/usb_change.py
+#
+# Tenable OT Security – UsbChange GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/usbchange.doc.html
+#
+# Purpose:
+# - Indicates the type of USB device change detected on a system
+
+USB_CHANGE_ENUM_NAME = "UsbChange"
+
+USB_CHANGE = [
+    "Removal",
+    "Insertion",
+]
