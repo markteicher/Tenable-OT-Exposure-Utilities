59e7156b1b2d1b6eaee5a7ce2cdaa0d8155865ce
Mark Teicher <mark.teicher@gmail.com>
2026-01-04 10:29:02 -0500

    Create tag_type.py

---
+++ queries/tag_type.py
@@ -0,0 +1,22 @@
+# queries/tag_type.py
+#
+# Tenable OT Security – TagType GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/tagtype.doc.html
+#
+# Purpose:
+# - Defines possible tag data types
+
+TAG_TYPE_ENUM_NAME = "TagType"
+
+TAG_TYPE = [
+    "Unknown",
+    "Int",
+    "Bool",
+    "Short",
+    "DInt",
+    "Long",
+    "Float",
+    "MultipleTagTypes",
+]
