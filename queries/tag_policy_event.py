b8543d11da40df6bf8d256e4aaf9bd2b74e7ef08
Mark Teicher <mark.teicher@gmail.com>
2026-01-04 10:16:26 -0500

    Create tag_policy_event.py

---
+++ queries/tag_policy_event.py
@@ -0,0 +1,15 @@
+# queries/tag_policy_event.py
+#
+# Tenable OT Security – TagPolicyEvent GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/tagpolicyevent.doc.html
+#
+# Purpose:
+# - Defines possible events for Tag Policies
+
+TAG_POLICY_EVENT_ENUM_NAME = "TagPolicyEvent"
+
+TAG_POLICY_EVENT = [
+    "UnauthorizedTagWrite",
+]
