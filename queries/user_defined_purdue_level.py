3880815d90fb37b6d48d1941e457e03cd65d4663
Mark Teicher <mark.teicher@gmail.com>
2026-01-04 15:56:35 -0500

    Create user_defined_purdue_level.py

---
+++ queries/user_defined_purdue_level.py
@@ -0,0 +1,21 @@
+# queries/user_defined_purdue_level.py
+#
+# Tenable OT Security – UserDefinedPurdueLevel GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/userdefinedpurduelevel.doc.html
+#
+# Purpose:
+# - Represents user-defined Purdue levels for assets
+
+USER_DEFINED_PURDUE_LEVEL_ENUM_NAME = "UserDefinedPurdueLevel"
+
+USER_DEFINED_PURDUE_LEVEL = [
+    "UnknownLevel",
+    "Level0",
+    "Level1",
+    "Level2",
+    "Level3",
+    "Level4",
+    "_RemoveUserDefinedValue",
+]
