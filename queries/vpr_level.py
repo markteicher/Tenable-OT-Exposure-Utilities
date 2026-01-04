8c0120a08703cf187bdacdc5a85a18c55e70af70
Mark Teicher <mark.teicher@gmail.com>
2026-01-04 16:06:54 -0500

    Create vpr_level.py

---
+++ queries/vpr_level.py
@@ -0,0 +1,19 @@
+# queries/vpr_level.py
+#
+# Tenable OT Security – VprLevel GraphQL Enum
+#
+# Source documentation:
+# https://docs.tenable.com/OT-security/api/vprlevel.doc.html
+#
+# Purpose:
+# - Represents the Vulnerability Priority Rating (VPR) level
+
+VPR_LEVEL_ENUM_NAME = "VprLevel"
+
+VPR_LEVEL = [
+    "None",
+    "Low",
+    "Medium",
+    "High",
+    "Critical",
+]
